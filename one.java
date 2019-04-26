8c53819d95f59e3d274c547900abe595ddd3457d
zero25x <38110923+zero25x@users.noreply.github.com>
2019-04-26 11:49:24 +0200

    Update one.java

--- one.java
+++ one.java
@@ -1,5 +1,17 @@
+import java.io.*;
+import java.io.IOException;
+
 public class one{
 
-
+  public static void main(String args[]) throws IOException{
+    
+    System.out.println("hi five");
+    InputStreamReader r=new InputStreamReader(System.in);    
+    BufferedReader br=new BufferedReader(r);            
+    System.out.println("Enter your name");    
+    String name=br.readLine();    
+    System.out.println("Welcome "+name);    
+    
+  }
 
 }
