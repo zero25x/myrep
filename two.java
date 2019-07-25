e2c552ce3d7acd636dcf0ab117af3cc74b74278c
zero25x <38110923+zero25x@users.noreply.github.com>
2019-07-25 12:31:47 +0200

    Update two.java

--- two.java
+++ two.java
@@ -1,1 +1,27 @@
+import java.io.*;
+import java.io.IOException;
 
+public class one{
+
+  public static void main(String args[]) throws IOException{
+    
+    System.out.println("hi five");
+    InputStreamReader r=new InputStreamReader(System.in);    
+    BufferedReader br=new BufferedReader(r);            
+    System.out.println("Enter your name");    
+    String name=br.readLine();    
+    System.out.println("Welcome "+name);
+    
+    switch(number){
+    
+      case 1:
+        System.out.println("Dodawanie danych Klienta");
+        System.our.println("Podaj nazwę Kl");
+        String nazwa_kl = br.readLine();
+        
+      case 2:
+    
+    }
+  }
+
+}
